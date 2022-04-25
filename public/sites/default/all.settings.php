26273c86229ed1eb6ab6198192c691139fe3897b
veikkoa <veikko.aitosalo@citrus.fi>
2022-04-25 09:39:23 +0300

    HIS-253: Disable itok output

--- public/sites/default/all.settings.php
+++ public/sites/default/all.settings.php
@@ -13,17 +13,26 @@ if ($env = getenv('APP_ENV')) {
     $config['elasticsearch_connector.cluster.local']['options']['use_authentication'] = '1';
     $config['elasticsearch_connector.cluster.local']['options']['username'] = 'elastic';
     $config['elasticsearch_connector.cluster.local']['options']['password'] = '11U1i6GY6I1Tmr829jQLo8dY';
+    // This should not be necessary
+    $config['image.settings']['suppress_itok_output'] = TRUE;
+    $config['image.settings']['allow_insecure_derivatives'] = TRUE;
   }
   if ($env == 'test') {
     $config['elasticsearch_connector.cluster.local']['url'] = 'https://elasticsearch-helhist-test-es-http:9200';
     $config['elasticsearch_connector.cluster.local']['options']['use_authentication'] = '1';
     $config['elasticsearch_connector.cluster.local']['options']['username'] = 'elastic';
     $config['elasticsearch_connector.cluster.local']['options']['password'] = 'Q7n88J667AHiEy7Z8J0G0ehD';
+    // This should not be necessary
+    $config['image.settings']['suppress_itok_output'] = TRUE;
+    $config['image.settings']['allow_insecure_derivatives'] = TRUE;
   }
   if ($env == 'production') {
     $config['elasticsearch_connector.cluster.local']['url'] = 'https://elasticsearch-helhist-prod-es-http:9200';
     $config['elasticsearch_connector.cluster.local']['options']['use_authentication'] = '1';
     $config['elasticsearch_connector.cluster.local']['options']['username'] = 'elastic';
     $config['elasticsearch_connector.cluster.local']['options']['password'] = '24r4BwuWWs6809EmNk30Y2ze';
+    // This should not be necessary
+    $config['image.settings']['suppress_itok_output'] = TRUE;
+    $config['image.settings']['allow_insecure_derivatives'] = TRUE;
   }
 }

18b4e96f86343eb1e14b00a6205cb3205d25e96a
veikkoa <veikko.aitosalo@citrus.fi>
2022-06-15 14:36:37 +0300

    HIS-264: HIS-264: Clean up duplicate functions

--- public/modules/custom/helhist_map/src/Plugin/Block/ComparisonMapControlsBlock.php
+++ public/modules/custom/helhist_map/src/Plugin/Block/ComparisonMapControlsBlock.php
@@ -61,8 +61,8 @@ class ComparisonMapControlsBlock extends BlockBase implements ContainerFactoryPl
   public function build() {
     $build = [
       '#theme' => 'comparison_map_controls',
-      '#map_layers' => $this->mapService->getMapLayers(),
-      '#photo_layers' => $this->mapService->getPhotoLayers()
+      '#map_layers' => $this->mapService->getMapLayers('map'),
+      '#photo_layers' => $this->mapService->getMapLayers('photo')
     ];
 
     return $build;

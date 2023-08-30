6450717abb24af5346dec5970731f276e83b6f7d
veikkoa <veikko.aitosalo@citrus.fi>
2023-08-30 15:24:51 +0300

    HIS-249: Fix wrong variable

--- public/sites/default/all.settings.php
+++ public/sites/default/all.settings.php
@@ -5,11 +5,11 @@
  * Contains site specific overrides.
  */
 
-// Openshift Elasticsearch authentication
-if ($env = getenv('APP_ENV')) {
+// Elasticsearch settings.
+if ($env = getenv('ELASTICSEARCH_URL')) {
   $config['elasticsearch_connector.cluster.local']['url'] = getenv('ELASTICSEARCH_URL');
   $config['elasticsearch_connector.cluster.local']['options']['use_authentication'] = '1';
-  $config['elasticsearch_connector.cluster.local']['options']['username'] = getenv('ELASTICSEARCH_URL');
+  $config['elasticsearch_connector.cluster.local']['options']['username'] = getenv('ELASTIC_USER');
   $config['elasticsearch_connector.cluster.local']['options']['password'] = getenv('ELASTIC_PASSWORD');
   // This should not be necessary
   $config['image.settings']['suppress_itok_output'] = TRUE;

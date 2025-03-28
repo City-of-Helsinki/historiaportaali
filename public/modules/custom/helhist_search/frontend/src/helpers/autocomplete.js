5641aae1c208573c1db052d6dfcc9cd9bc05a4c2
Timo Kirkkala <timo.kirkkala@digitalistgroup.com>
2025-03-28 08:58:30 +0200

    UHF-11461 / HIS-313: Update search_api_autocomplete, change autocomplete to use live results instead of broken server search

--- public/modules/custom/helhist_search/frontend/src/helpers/autocomplete.js
+++ public/modules/custom/helhist_search/frontend/src/helpers/autocomplete.js
@@ -2,24 +2,26 @@ import { API_URL } from '../constants/constants.js';
 
 const fetchAutocompleteSuggestions = async (searchQuery) => {
   try {
-    const formData = new FormData();
-    formData.append("s", searchQuery);
+    const params = new URLSearchParams({
+      q: searchQuery.trim()
+    });
 
-    const res = await fetch(`${API_URL}/search_api_autocomplete/search?display=page&filter=s&q=${searchQuery}`, {
+    const res = await fetch(`${API_URL}/search_api_autocomplete/search?${params}`, {
       method: "POST",
-      body: formData
+      headers: {
+        'Content-Type': 'application/json'
+      }
     });
 
     if (!res.ok) {
-      const message = `An error has occured: ${res.status} - ${res.statusText}`;
-      throw new Error(message);
+      throw new Error(`HTTP error with status ${res.status}`);
     }
 
     const data = await res.json();
-
     return data;
   } catch (err) {
-    console.log('Error: ' + err.message);
+    console.error('Autocomplete fetch error:', err);
+    throw err;
   }
 }
 

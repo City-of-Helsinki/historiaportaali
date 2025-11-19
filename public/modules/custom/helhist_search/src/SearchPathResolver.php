ac5fcef590c7c54fea86c250ec3a0b020445696b
Timo Kirkkala <timo.kirkkala@digitalistgroup.com>
2025-11-19 17:30:20 +0200

    HIS-330: Refactor theme to use SearchPathResolver

--- public/modules/custom/helhist_search/src/SearchPathResolver.php
+++ public/modules/custom/helhist_search/src/SearchPathResolver.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Drupal\helhist_search;
 
-use Drupal\Core\Language\LanguageManagerInterface;
+use Drupal\Core\Url;
 
 /**
  * Resolves search page paths based on language.
@@ -12,36 +12,19 @@ use Drupal\Core\Language\LanguageManagerInterface;
 class SearchPathResolver {
 
   /**
-   * The language manager service.
-   *
-   * @var \Drupal\Core\Language\LanguageManagerInterface
+   * The node ID of the main search page.
    */
-  protected $languageManager;
+  const SEARCH_PAGE_NODE_ID = 114;
 
   /**
-   * Constructs a new SearchPathResolver object.
-   *
-   * @param \Drupal\Core\Language\LanguageManagerInterface $language_manager
-   *   The language manager service.
-   */
-  public function __construct(LanguageManagerInterface $language_manager) {
-    $this->languageManager = $language_manager;
-  }
-
-  /**
-   * Get the search page path based on current language.
+   * Get the search page path.
    *
    * @return string
    *   The search page path.
    */
   public function getSearchPagePath(): string {
-    $langcode = $this->languageManager->getCurrentLanguage()->getId();
-
-    return match ($langcode) {
-      'sv' => '/sv/sok',
-      'en' => '/en/search',
-      default => '/fi/haku',
-    };
+    $url = Url::fromRoute('entity.node.canonical', ['node' => self::SEARCH_PAGE_NODE_ID]);
+    return $url->toString();
   }
 
 }

809a7ad330f53580b95fd386e4431453c8ba2dc1
Santeri Hurnanen <santeri.hurnanen@fame.fi>
2026-03-20 15:05:18 +0200

    Map ad groups to roles

--- public/sites/default/all.settings.php
+++ public/sites/default/all.settings.php
@@ -10,6 +10,25 @@ if (PHP_SAPI === 'cli') {
   ini_set('memory_limit', '1024M');
 }
 
+$config['openid_connect.client.tunnistamo']['settings']['ad_roles'] = [
+  [
+    'ad_role' => 'Drupal_Helfi_kaupunkitaso_paakayttajat',
+    'roles' => ['admin'],
+  ],
+  [
+    'ad_role' => 'Drupal_Helfi_historiaportaali_sisallontuottajat_laaja',
+    'roles' => ['editor'],
+  ],
+  [
+    'ad_role' => 'Drupal_Helfi_historiaportaali_sisallontuottajat_suppea',
+    'roles' => ['content_producer'],
+  ],
+  [
+    'ad_role' => '947058f4-697e-41bb-baf5-f69b49e5579a',
+    'roles' => ['super_administrator'],
+  ],
+];
+
 if ($elastic_proxy_url = getenv('ELASTIC_PROXY_URL')) {
   $settings['elasticsearch_proxy_url'] = $elastic_proxy_url;
 }

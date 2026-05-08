b41bfa03195642ecb28f16b266cca9e00bb1e96d
Toni Sinisalo <toni.sinisalo@digitalistgroup.com>
2026-05-08 14:59:39 +0300

    Removing mappingMode from type definitions.

--- public/themes/custom/hdbt_subtheme/src/js/types/drupalSettings.d.ts
+++ public/themes/custom/hdbt_subtheme/src/js/types/drupalSettings.d.ts
@@ -2,10 +2,6 @@ declare namespace drupalSettings {
   const koreSearch: {
     typeOptions: Array<{ value: string; label: string }>;
     languageOptions: Array<{ value: string; label: string }>;
-    mappingMode: 'text' | 'keyword';
     sortLabels: Record<string, string>;
-  };
-  const search: {
-    mappingMode: 'text' | 'keyword';
   };
 }
